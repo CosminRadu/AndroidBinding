cbf4285259e568d510ec821f0fd2e49350a17525
Cosmin Radu <Cosmin.Radu@microsoft.com>
2015-02-18 12:44:33 -0800

    Stop gap measure for generating *one* JAR file from the outputs of the 'androidbinding' and 'androidbindingv30' projects.

---
+++ Core/androidbindingJar/src/main/java/gueei/binding/Stub.java
@@ -0,0 +1,14 @@
+package gueei.binding;
+
+import gueei.binding.app.BindingActivity;
+import gueei.binding.v30.app.BindingActivityV30;
+
+/**
+ * Created by cosminr on 2/3/2015.
+ */
+public class Stub {
+    private Stub() {
+        BindingActivity ba;
+        BindingActivityV30 bav3;
+    }
+}
